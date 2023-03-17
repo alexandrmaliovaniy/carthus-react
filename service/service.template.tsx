496be0ca9344b03943ccf198212a7c0b6441cd19
Alexandr Maliovaniy <alexandrmaliovaniy@gmail.com>
2023-03-17 15:47:41 +0200

    v1.0.3

--- service/service.template.tsx
+++ service/service.template.tsx
@@ -1,6 +1,6 @@
 
 interface I{{ARGS.NAME}}ServiceProps {}
 
-const {{ARGS.NAME}}Service = ({...porps}?: I{{ARGS.NAME}}ServiceProps) => {
+const {{ARGS.NAME}}Service = (props?: I{{ARGS.NAME}}ServiceProps) => {
 
 };
